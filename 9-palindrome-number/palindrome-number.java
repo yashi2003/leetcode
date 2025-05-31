57b4a06703ae89a0ae77ba54a1ca611b12d8ef4b
yashi2003 <111288929+yashi2003@users.noreply.github.com>
2025-05-31 12:46:40 +0530

    Time: 5 ms (70.00%) | Memory: 44.1 MB (62.36%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev = 0;
+        int xcopy = x;
+        while(x>0){
+            rev = (rev*10) + (x%10);
+            x = x/10;
+        }
+        return rev == xcopy;
+    }
+}
